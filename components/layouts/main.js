b4307967946dcc3b4b4b6b2f68e7b2f52a8c46eb
Andrew Heim <andrewsheim@gmail.com>
2023-02-18 15:21:26 -0800

    Implement Chakra UI

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,10 +1,18 @@
 import Head from 'next/head'
-import { Box } from '@chakra-ui/react'
+import { Box, Container } from '@chakra-ui/react'
 
-const Main = ({ children, router}) => {
+const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
-
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Andrew Heim - Home Page</title>
+      </Head>
+      <Container maxW="container.md" pt={14}>
+        {children}
+      </Container>
     </Box>
   )
 }
+
+export default Main
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
 const Page = () => {
   return (
-    <div>Hello</div>
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a full-stack developer based in California!
+      </Box>
+      <Box display={{ md: 'flex'}}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+          </Heading>
+          <p>Digital Creator ( Videographer / Developer )</p>
+        </Box>
+      </Box>
+    </Container>
   )
 }
 
